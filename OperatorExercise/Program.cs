87b6a70710fa15030b57b020f583d92137ec881a
Devin Riley <devin.riley144@gmail.com>
2022-12-28 13:53:16 -0600

    added area calc.

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace OperatorExercise
+﻿using System.Net.NetworkInformation;
+using System.Reflection.Metadata.Ecma335;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            var answer = methods.AreaOfCircle( radius );
+            Console.WriteLine(answer);
         }
+       
+        
+
     }
+
 }
+
+        
---
+++ OperatorExercise/methods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    internal class methods
+    {
+        public static double AreaOfCircle(double radius)
+       {
+
+        var area = Math.PI * Math.Pow(radius, 2);
+        return area; 
+
+
+        }
+
+
+    }
+}
